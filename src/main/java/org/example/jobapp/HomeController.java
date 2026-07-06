820cd6d8419e24377baca7a826b24eb171fdd70e
rajkumar-131 <rajkumar.rk.13113@gmail.com>
2026-07-06 19:12:26 +0530

    Changed the URL for the HomeController

--- src/main/java/org/example/jobapp/HomeController.java
+++ src/main/java/org/example/jobapp/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController
 {
-    @GetMapping("/")
+    @GetMapping("/homeController")
     public String home() {
         return "JobApp is running successfully 🚀";
     }
