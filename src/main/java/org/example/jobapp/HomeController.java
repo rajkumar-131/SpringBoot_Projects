8e7f5389a993a05777e8bb6db77e3c8ed4b1da61
rajkumar-131 <rajkumar.rk.13113@gmail.com>
2026-07-06 19:16:44 +0530

    Removed HomeController Mapping

--- src/main/java/org/example/jobapp/HomeController.java
+++ src/main/java/org/example/jobapp/HomeController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController
 {
-    @GetMapping("/homeController")
-    public String home() {
-        return "JobApp is running successfully 🚀";
-    }
+//    @GetMapping("/homeController")
+//    public String home() {
+//        return "JobApp is running successfully 🚀";
+//    }
 }
